14b20fe57c2e0302de682dba7fa55ebb7352f367
Edgar Mueller <emueller@eclipsesource.com>
2016-01-18 15:13:53 +0100

    Solution to exercise 1

---
+++ app/album/album.directive.ts
@@ -0,0 +1,34 @@
+/// <reference path='../_references.ts' />
+
+class AlbumDirective implements ng.IDirective {
+
+    restrict = 'E';
+
+    template = `<ul ng-repeat='album in albums'>
+      <li>
+          {{album.name}}
+          <ol>
+            <li ng-repeat="song in album.tracks">
+              {{song}}
+            </li>  
+          </ol>  
+      </li>
+    </ul>`;
+    
+    scope = {
+        artistId: "="
+    };
+
+    link = (scope, element, attributes) => {
+        let artistId = scope.artistId;
+        scope.albums = this.dataService.albums.filter(a => a.artistId == artistId);
+    };
+
+    constructor(private dataService: DataService) {
+    }
+}
+
+angular.module('econTutorial')
+    .directive('albumList', ['DataService', (DataService) => {
+        return new AlbumDirective(DataService);
+    }]);
