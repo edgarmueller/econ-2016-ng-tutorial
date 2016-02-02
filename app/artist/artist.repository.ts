7a595b02a36e97a7da02890f8f4d8134ce1c9e31
Edgar Mueller <emueller@eclipsesource.com>
2016-02-02 12:43:43 +0100

    Fixed compile error in artist repo

--- app/artist/artist.repository.ts
+++ app/artist/artist.repository.ts
@@ -3,11 +3,11 @@
 class ArtistRepository extends InMemoryRepository<Artist> {
 
     fillWithSampleData() {
-        this.entities.create(new Artist("Amy Winehouse",          "images/artists/winehouse.jpg"));
-        this.entities.create(new Artist("Portishead",             "images/artists/portishead"));
-        this.entities.create(new Artist("The Velvet Underground", "images/artists/the-velvet-underground.jpg"));
-        this.entities.create(new Artist("Roxy Music",             "images/artists/roxy-music.jpg"));
-        this.entities.create(new Artist("The Doors",              "images/artists/the-doors.jpg"));
+        this.create(new Artist("Amy Winehouse",          "images/artists/winehouse.jpg"));
+        this.create(new Artist("Portishead",             "images/artists/portishead"));
+        this.create(new Artist("The Velvet Underground", "images/artists/the-velvet-underground.jpg"));
+        this.create(new Artist("Roxy Music",             "images/artists/roxy-music.jpg"));
+        this.create(new Artist("The Doors",              "images/artists/the-doors.jpg"));
     }
 
 }
