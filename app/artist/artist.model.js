1012878367b7f4d9fb411a2b58a3225492c4d3b1
Edgar Mueller <emueller@eclipsesource.com>
2016-02-11 10:39:37 +0100

    exercise 1 preparation

--- app/artist/artist.model.js
+++
@@ -1,9 +0,0 @@
-var Artist = (function () {
-    function Artist(name, imagePath, id) {
-        this.name = name;
-        this.imagePath = imagePath;
-        this.id = id;
-    }
-    return Artist;
-})();
-//# sourceMappingURL=artist.model.js.map
