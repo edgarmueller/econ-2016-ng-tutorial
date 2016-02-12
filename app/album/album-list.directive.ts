2f7935bbf4a01fdcfbd4a286b40bd6ba93c79b49
Edgar Mueller <emueller@eclipsesource.com>
2016-02-12 12:26:20 +0100

    Added type annotations

--- app/album/album-list.directive.ts
+++ app/album/album-list.directive.ts
@@ -1,25 +1,25 @@
 /// <reference path='../_references.ts' />
 
 class AlbumListDirectiveController {
-    public artistId;
+    public artistId: number;
     static $inject = ['AlbumRepository'];
     constructor(private repo: AlbumRepository) {}
-    albums(): Album[] {
+    getAlbums(): Album[] {
         return this.repo.getAll().filter(album => album.artistId == this.artistId);
     }
 }
 
 class AlbumListDirective implements ng.IDirective {
 
-    restrict = 'E';
+    restrict: string = 'E';
 
     // to reference the controller's properties in the template
-    controllerAs = 'vm';
+    controllerAs: string = 'vm';
 
     // type refinement, otherwise we'll get an compile error in the constructor while assigning the controller
     controller = 'AlbumListDirectiveController';
 
-    template = `<div ng-repeat="album in vm.albums()">
+    template: string = `<div ng-repeat="album in vm.getAlbums()">
       <h2>{{album.name}}</h2>
       <ol>
         <li ng-repeat="track in album.tracks">
@@ -28,7 +28,7 @@ class AlbumListDirective implements ng.IDirective {
       </ol>
     </div>`;
 
-    bindToController = {
+    bindToController: Object = {
         'artistId': '='
     };
 }
